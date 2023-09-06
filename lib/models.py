907fd4f9039173c1a46d2aed2f55dbff785b29dc
Ian <ian.odhiambo@student.moringaschool.com>
2023-09-06 15:26:12 +0300

    Transaction

--- lib/models.py
+++ lib/models.py
@@ -1,19 +1,36 @@
-from sqlalchemy import ForeignKey , Column , Integer , String , MetaData
-from sqlalchemy import relationship ,backref
+from sqlalchemy import   Column , Integer , String ,ForeignKey, create_engine
+from sqlalchemy.orm import relationship , backref
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.associationproxy import association_proxy
 
-convection = {
-    "fk":"fk_%(table_name)s_%(column_o_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convection)
 
-Base = declarative_base(metadata=metadata)
 
-class transations (Base):
+Base = declarative_base()
 
+class User (Base):
+    __tablename__= 'users'
+
+    id = Column (Integer(), primary_key=True)
+    name = Column (String())
+
+    transactions = relationship('Transaction' , backref='user')
+
+class Transaction(Base):
     __tablename__ = 'transactions'
 
-    transactions.id = Column(Integer , primary_keys=True)
+    id = Column(Integer() , primary_key=True)
+    amount = Column (Integer())
+    user_id = Column(Integer() , ForeignKey('users.id'))
+
+
+# engine = create_engine ('sqlite:///transaction.db')
+# create a databaase engine 
+
+
+# Base.metadata.create_all(engine) 
+# Create the tables in the database
+
+
+
+
 
 
